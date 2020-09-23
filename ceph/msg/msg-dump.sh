cc0a8f364cf716bb2136f9550bc9f943732e734e
Yanhu Cao <gmayyyha@gmail.com>
2020-09-23 17:30:29 +0800

    ceph/msg: add dump msg filter
    e.g.
    ./msg-dump.sh -m CEPH_MSG_CLIENT_CAPS ceph-fuse
    ./msg-dump.sh -p 24803 -m MSG_ROUTE ceph-mon
    
    Signed-off-by: Yanhu Cao <gmayyyha@gmail.com>

--- ceph/msg/msg-dump.sh
+++ ceph/msg/msg-dump.sh
@@ -46,29 +46,44 @@ shift $(($OPTIND - 1))
 entity=$1
 case $entity in
 ceph-mon)
-        stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
-        provider='process(@BIN_MON).library("libceph-common.so.*")'
-        ;;
+	stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
+	if [ "x$pid" != "x" ]; then
+		stapopt="-x $pid $stapopt"
+	fi
+	provider='process(@BIN_MON).library("libceph-common.so.*")'
+	;;
 
 ceph-mgr)
-        stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
-        provider='process(@BIN_MGR).library("libceph-common.so.*")'
-        ;;
+	stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
+	if [ "x$pid" != "x" ]; then
+		stapopt="-x $pid $stapopt"
+	fi
+	provider='process(@BIN_MGR).library("libceph-common.so.*")'
+	;;
 
 ceph-osd)
-        stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
-        provider='process(@BIN_OSD)'
-        ;;
+	stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
+	if [ "x$pid" != "x" ]; then
+		stapopt="-x $pid $stapopt"
+	fi
+	provider='process(@BIN_OSD)'
+	;;
 
 ceph-mds)
-        stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
-        provider='process(@BIN_MDS).library("libceph-common.so.*")'
+	stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
+	if [ "x$pid" != "x" ]; then
+		stapopt="-x $pid $stapopt"
+	fi
+	provider='process(@BIN_MDS).library("libceph-common.so.*")'
         ;;
 
 ceph-fuse)
-        stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
-        provider='process(@BIN_FUSE).library("libceph-common.so.*")'
-        ;;
+	stapopt="--ldd -I $TAPSET_CEPH_PATH $stapopt"
+	if [ "x$pid" != "x" ]; then
+		stapopt="-x $pid $stapopt"
+	fi
+	provider='process(@BIN_FUSE).library("libceph-common.so.*")'
+	;;
 
 *)
         usage
@@ -83,47 +98,53 @@ global lasttime;
 probe   '$provider'.function("DispatchQueue::enqueue@*/src/msg/DispatchQueue.cc")
 ,       '$provider'.function("DispatchQueue::fast_dispatch@*/src/msg/DispatchQueue.cc")
 {
-        msg = $m->px;
-	ms_type = msg->header->type->v;
-        addr = msg->connection->px->peer_addrs->current;
-        peeraddr = addr->v->_M_impl->_M_start->u->sin->sin_addr->s_addr;
-        port = addr->v->_M_impl->_M_start->u->sin->sin_port;
-        type = msg->connection->px->peer_type;
-        nonce = addr->v->_M_impl->_M_start->nonce;
+	msg = $m->px;
+	if ("'$dumpmsg'" == "" || ceph_msg_getname(msg) =~ "'$dumpmsg'") {
+		if (target() == pid() || "'$pid'" == "") {
+			ms_type = msg->header->type->v;
+			addr = msg->connection->px->peer_addrs->current;
+			peeraddr = addr->v->_M_impl->_M_start->u->sin->sin_addr->s_addr;
+			port = addr->v->_M_impl->_M_start->u->sin->sin_port;
+			type = msg->connection->px->peer_type;
+			nonce = addr->v->_M_impl->_M_start->nonce;
 
+			ansi_set_color(32);
+			printf("%18d  %15s/%d  %-30s  %-35s  %-5d  <== %8s  %-s:%-d/%-d\n",
+			        gettimeofday_us() - lasttime[pid(), peeraddr, port, type, nonce],
+			        execname(), pid(), ppfunc(), ceph_msg_getname(msg), msg->header->priority->v,
+			        ceph_entity_type_name(type),
+			        format_ipaddr(peeraddr, 2), ntohs(port), nonce);
+			ceph_msg_dump(msg);
+			ansi_reset_color();
 
-        ansi_set_color(32);
-        printf("%18d  %15s/%d  %-30s  %-35s  %-5d  <-- %8s  %-s:%-d/%-d\n",
-                gettimeofday_us() - lasttime[pid(), peeraddr, port, type, nonce],
-                execname(), pid(), ppfunc(), ceph_msg_getname(msg), msg->header->priority->v,
-                ceph_entity_type_name(type),
-                format_ipaddr(peeraddr, 2), ntohs(port), nonce);
-        ceph_msg_dump(msg);
-        ansi_reset_color();
-
-        lasttime[pid(), peeraddr, port, type, nonce] = gettimeofday_us();
+			lasttime[pid(), peeraddr, port, type, nonce] = gettimeofday_us();
+		}
+	}
 }
 
 probe   '$provider'.function("*::write_message")
 {
-        msg = $m;
-	ms_type = msg->header->type->v;
-        addr = msg->connection->px->peer_addrs->current;
-        peeraddr = addr->v->_M_impl->_M_start->u->sin->sin_addr->s_addr;
-        port = addr->v->_M_impl->_M_start->u->sin->sin_port;
-        type = msg->connection->px->peer_type;
-        nonce = addr->v->_M_impl->_M_start->nonce;
+	msg = $m;
+	if ("'$dumpmsg'" == "" || ceph_msg_getname(msg) =~ "'$dumpmsg'") {
+		if (target() == pid() || "'$pid'" == "") {
+			ms_type = msg->header->type->v;
+			addr = msg->connection->px->peer_addrs->current;
+			peeraddr = addr->v->_M_impl->_M_start->u->sin->sin_addr->s_addr;
+			port = addr->v->_M_impl->_M_start->u->sin->sin_port;
+			type = msg->connection->px->peer_type;
+			nonce = addr->v->_M_impl->_M_start->nonce;
 
+			ansi_set_color(31);
+			printf("%18d  %15s/%d  %-30s  %-35s  %-5d  --> %8s  %-s:%-d/%-d\n",
+			        gettimeofday_us() - lasttime[pid(), peeraddr, port, type, nonce],
+			        execname(), pid(), ppfunc(), ceph_msg_getname(msg), msg->header->priority->v,
+			        ceph_entity_type_name(type),
+			        format_ipaddr(peeraddr, 2), ntohs(port), nonce);
+			ceph_msg_dump(msg);
+			ansi_reset_color();
 
-        ansi_set_color(31);
-        printf("%18d  %15s/%d  %-30s  %-35s  %-5d  ==> %8s  %-s:%-d/%-d\n",
-                gettimeofday_us() - lasttime[pid(), peeraddr, port, type, nonce],
-                execname(), pid(), ppfunc(), ceph_msg_getname(msg), msg->header->priority->v,
-                ceph_entity_type_name(type),
-                format_ipaddr(peeraddr, 2), ntohs(port), nonce);
-        ceph_msg_dump(msg);
-        ansi_reset_color();
-
-        lasttime[pid(), peeraddr, port, type, nonce] = gettimeofday_us();
+			lasttime[pid(), peeraddr, port, type, nonce] = gettimeofday_us();
+		}
+	}
 }
 ' | c++filt
